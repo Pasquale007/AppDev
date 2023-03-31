3860a9801c3aafe9647b5a7609636a5556372fd1
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-03-31 21:40:36 +0200

    mock files so that the test runs through
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
