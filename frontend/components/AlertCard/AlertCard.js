2b90685f05f6b622601336ca13dae65b3675fb39
Orchi1904 <alexej1@web.de>
2023-04-03 22:36:00 +0200

    Finished AlertPage design

---
+++ frontend/components/AlertCard/AlertCard.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import styles from "./AlertCard.style";
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Switch } from 'react-native-switch';
+import { Swipeable } from 'react-native-gesture-handler';
+
+import { COLORS } from "../../constants/theme";
+import { Ionicons } from '@expo/vector-icons';
+
+function AlertCard({ date, locations, maxPrice, index, closeCard, cardArr }) {
+    const [isEnabled, setIsEnabled] = useState(true);
+
+    const renderRightActions = () => {
+        return (
+            <TouchableOpacity style={styles.deleteButton}>
+                <Ionicons style={styles.trashIcon} size={30} name="trash" />
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <View style={styles.container}>
+            <Swipeable
+                renderRightActions={renderRightActions}
+                onSwipeableOpen={() => closeCard(index)}
+                ref={(ref) => (cardArr[index] = ref)}
+            >
+            {/*numberOfLines={1} adjustsFontSizeToFit={true}*/}
+                <View style={styles.alertCard}>
+                    <View style={styles.leftSide}>
+                        <Text style={styles.dateText}>
+                            {date.start} - {date.end}
+                        </Text>
+                        <Text style={styles.departureText} numberOfLines={1} ellipsizeMode='tail'>
+                            {locations.departure}
+                        </Text>
+                        <Text style={styles.arrivalText} numberOfLines={1} ellipsizeMode='tail'>
+                            {locations.arrival}
+                        </Text>
+                    </View>
+                    <View style={styles.rightSide}>
+                        <Text style={styles.maxPriceText} numberOfLines={1} adjustsFontSizeToFit={true}>
+                            {maxPrice.toLocaleString("de-DE", { style: "currency", currency: "EUR" })}
+                        </Text>
+                        <View style={styles.toggleButtonContainer}>
+                            <Switch
+                                value={isEnabled}
+                                onValueChange={() => setIsEnabled(!isEnabled)}
+                                circleSize={20}
+                                circleBorderWidth={0}
+                                backgroundActive={COLORS.switchActive}
+                                backgroundInactive={COLORS.switchInactive}
+                                changeValueImmediately={true}
+                                renderActiveText={false}
+                                renderInActiveText={false}
+                            />
+                        </View>
+                    </View>
+                </View>
+            </Swipeable>
+            <View style={styles.cardBackground} />
+        </View>
+    )
+}
+
+export default AlertCard;
