09e8d6e18e2a9d3666fd53cd471ccec2a35c0948
Marc <marc.ruddigkeit@gmx.net>
2023-04-08 13:31:48 +0200

    Changed nothing

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -30,31 +30,32 @@ export default function App() {
   };
 
   return (
-    <NavigationContainer >
-   <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarHideOnKeyboard: true,
-        headerShown: false,
-        tabBarActiveTintColor: COLORS.navIconActive,
-        tabBarInactiveTintColor: COLORS.navIconInactive,
-        tabBarActiveBackgroundColor: COLORS.navigationBar,
-        tabBarInactiveBackgroundColor: COLORS.navigationBar,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'home'
-              : 'home-outline';
-          } else if (route.name === 'Alerts') {
-            iconName = focused ? "notifications" : "notifications-outline";
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}>
-      <Splash />
-      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-      <Tab.Screen name="Alerts" component={AlertPage} options={{ title: 'Alerts' }} />
-    </Tab.Navigator>
-    </NavigationContainer>
+    <><Splash /><NavigationContainer>
+
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarHideOnKeyboard: true,
+          headerShown: false,
+          tabBarActiveTintColor: COLORS.navIconActive,
+          tabBarInactiveTintColor: COLORS.navIconInactive,
+          tabBarActiveBackgroundColor: COLORS.navigationBar,
+          tabBarInactiveBackgroundColor: COLORS.navigationBar,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Alerts') {
+              iconName = focused ? "notifications" : "notifications-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
+
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Tab.Screen name="Alerts" component={AlertPage} options={{ title: 'Alerts' }} />
+      </Tab.Navigator>
+    </NavigationContainer></>
   );
 };
