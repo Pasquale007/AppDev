585762b022fc6f1b27012747c30f897954c673fd
Marc <marc.ruddigkeit@gmx.net>
2023-04-08 13:03:08 +0200

    Tried to fix the splash-screen, not working

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -30,32 +30,31 @@ export default function App() {
   };
 
   return (
-    <><Splash /><NavigationContainer
-      theme={MyTheme}
-    >
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarHideOnKeyboard: true,
-          headerShown: false,
-          tabBarActiveTintColor: COLORS.navIconActive,
-          tabBarInactiveTintColor: COLORS.navIconInactive,
-          tabBarActiveBackgroundColor: COLORS.navigationBar,
-          tabBarInactiveBackgroundColor: COLORS.navigationBar,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'Alerts') {
-              iconName = focused ? "notifications" : "notifications-outline";
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}>
-        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-        <Tab.Screen name="Alerts" component={AlertPage} options={{ title: 'Alerts' }} />
-      </Tab.Navigator>
-    </NavigationContainer></>
+    <NavigationContainer >
+   <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.navIconActive,
+        tabBarInactiveTintColor: COLORS.navIconInactive,
+        tabBarActiveBackgroundColor: COLORS.navigationBar,
+        tabBarInactiveBackgroundColor: COLORS.navigationBar,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          } else if (route.name === 'Alerts') {
+            iconName = focused ? "notifications" : "notifications-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}>
+        <Splash />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+      <Tab.Screen name="Alerts" component={AlertPage} options={{ title: 'Alerts' }} />
+    </Tab.Navigator>
+    </NavigationContainer>
   );
 };
