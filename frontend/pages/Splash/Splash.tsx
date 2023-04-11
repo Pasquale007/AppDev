5d3c978cd13287acdc1b0d00f365d722a1f4b956
Marc <marc.ruddigkeit@gmx.net>
2023-04-11 10:13:34 +0200

    Animated like in a tutorial, but does not show up at start

--- frontend/pages/Splash/Splash.tsx
+++ frontend/pages/Splash/Splash.tsx
@@ -1,18 +1,19 @@
 import * as React from "react";
 import { View } from "react-native";
-import LottieView from "lottie-react-native"
+import {LottieView } from "lottie-react-native";
 import { SetStateAction } from "react";
+import Lottie from "lottie-react-native";
 
 interface SplashProps{
   setIsLoading: React.Dispatch<SetStateAction<boolean>>;
 }
 
-export default function Splash({setIsLoading}:SplashProps):JSX.Element{
+/**export default function Splash({setIsLoading}:SplashProps):JSX.Element{
    return(
      <View style={{flex: 1, alignItems: 'center', margin: 0}}>
    <LottieView 
-   source={require("../../assets/splashscreen/ezgif.com-gif-to-mp4-latest.mp4.lottie.json")}
-   autoPlay
+   source={require("../../assets/splashscreen/splashscreen.json")}
+   autoPlay={true}
     loop={false}
   resizeMode="cover"
   onAnimationFinish={() => setIsLoading(false)}
@@ -20,12 +21,12 @@ export default function Splash({setIsLoading}:SplashProps):JSX.Element{
   </View>
  )
    }
+**/
 
 
-
-//export default function Animation() {
+export default function Animation({setIsLoading}:SplashProps):JSX.Element {
     
-  //  return (
-    //  <Lottie source={require('../../assets/splashscreen/ezgif.com-gif-to-mp4-latest.mp4.lottie.json')} autoPlay loop={false} />
-    //);
-  //}
+  return (
+  <Lottie source={require('../../assets/splashscreen/splashscreen.json')} autoPlay loop={false} />
+    );
+  }
