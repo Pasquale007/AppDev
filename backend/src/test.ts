db42d6f1237f3d566b7ecd8ad405d23f6207ed99
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-04-13 19:40:27 +0200

    fix for linter
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- backend/src/test.ts
+++ backend/src/test.ts
@@ -1,10 +1,10 @@
-const dateString1: string = "2022-04-15"; // first date in "YYYY-MM-DD" format
-const dateString2: string = "2022-04-20"; // second date in "YYYY-MM-DD" format
+const dateString1 = "2022-04-15"; // first date in "YYYY-MM-DD" format
+const dateString2 = "2022-04-20"; // second date in "YYYY-MM-DD" format
 
 const date1: Date = new Date(dateString1);
 const date2: Date = new Date(dateString2);
 
-if (new Date(dateString1) < new Date(dateString2)) {
+if (date1 < date2) {
     console.log(`${dateString1} is smaller than ${dateString2}.`);
 } else {
     console.log(`${dateString1} is not smaller than ${dateString2}.`);
