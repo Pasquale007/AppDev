524a966b9f4a0f1340a52799548d1b1d1ce86812
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-04-14 22:50:08 +0200

    fix code
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/pages/Splash/Splash.jsx
+++ frontend/pages/Splash/Splash.jsx
@@ -1,34 +1,19 @@
 import * as React from "react";
-import { View } from "react-native";
-import LottieView  from "lottie-react-native";
-import { SetStateAction } from "react";
-import Lottie from "lottie-react-native";
+import LottieView from "lottie-react-native";
+import GIF from "../../assets/splashscreen/splashscreen.json";
 
+export default function Splash({ setIsLoading }) {
 
-export default function Splash({setIsLoading}){
-  
-React.useEffect(()=>{
-  setTimeout(setIsLoading, 3000)
-},[])
+  React.useEffect(() => {
+    setTimeout(setIsLoading, 3000)
+  }, [])
 
-   return(
-     //<View style={{flex: 1, alignItems: 'center', margin: 0}}>
-   <LottieView 
-   source={require("../../assets/splashscreen/splashscreen.json")}
-   autoPlay={true}
-    loop={true}
-  resizeMode="cover"
-  />
- // </View>
- )
-   }
-
-
-/**
-export default function Animation({setIsLoading}) {
-    
   return (
-  <Lottie source={require('../../assets/splashscreen/splashscreen.json')} autoPlay loop={false} />
-    );
-  }
-**/
+    <LottieView
+      source={GIF}
+      autoPlay={true}
+      loop={true}
+      resizeMode="cover"
+    />
+  )
+}
