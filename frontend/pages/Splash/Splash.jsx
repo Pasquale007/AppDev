ed9a9459066450b478bd3d3df144ab129616a585
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-04-21 13:00:37 +0200

    init
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/pages/Splash/Splash.jsx
+++ frontend/pages/Splash/Splash.jsx
@@ -1,34 +1,21 @@
-import * as React from "react";
-import { View } from "react-native";
-import LottieView  from "lottie-react-native";
-import { SetStateAction } from "react";
-import Lottie from "lottie-react-native";
+import React from "react";
+import styles from "./Splash.styles";
+import videoFile from "../../assets/splashscreen/splash.mp4";
+import Video from 'react-native-video';
 
+export default function Splash({ setIsLoading }) {
+  React.useEffect(() => {
+    setTimeout(setIsLoading, 3000)
+  }, [])
 
-export default function Splash({setIsLoading}){
-  
-React.useEffect(()=>{
-  setTimeout(setIsLoading, 3000)
-},[])
-
-   return(
-     //<View style={{flex: 1, alignItems: 'center', margin: 0}}>
-   <LottieView 
-   source={require("../../assets/splashscreen/Splashscreen-new.json")}
-   autoPlay={true}
-    loop={true}
-  resizeMode="contain"
-  />
- // </View>
- )
-   }
-
-
-/**
-export default function Animation({setIsLoading}) {
-    
   return (
-  <Lottie source={require('../../assets/splashscreen/splashscreen.json')} autoPlay loop={false} />
-    );
-  }
-**/
+    <Video
+      source={videoFile}
+      style={styles.backgroundVideo}
+      muted={true}
+      repeat={true}
+      resizeMode={'cover'}
+      autoplay={true}
+    />
+  );
+}
---
+++ frontend/pages/Splash/Splash.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS } from "../../constants/theme";
+
+const styles = StyleSheet.create({
+    main: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        color: COLORS.background,
+    },
+});
+
+export default styles;
