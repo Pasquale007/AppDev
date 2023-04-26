4efea179ef90348a0b8c4352257a4d16557122e8
Orchi1904 <alexej1@web.de>
2023-04-26 18:20:46 +0200

    Added eslint-disable-next-line because global does not have to be defined

--- frontend/setup.js
+++ frontend/setup.js
@@ -1,5 +1,5 @@
 //Needed so tests can use crypto and wont crash
-//eslint-disable-next-line no-undef
 import { Crypto } from "@peculiar/webcrypto"
 
+//eslint-disable-next-line no-undef
 global.crypto = new Crypto()
