4b454b4637b6b5fd597f25591f2cc948bf42cb67
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-04-28 13:44:25 +0200

    fxi layout
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/pages/FlightResultPage/FlightResultPage.jsx
+++ frontend/pages/FlightResultPage/FlightResultPage.jsx
@@ -106,7 +106,7 @@ export default function FlightResultPage({ route }) {
                     />
                 </View>
             </ImageBackground>
-            <ToastContainer />
+            <ToastContainer/>
         </View>
     );
 }
