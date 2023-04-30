db7ed5cc9bc0ff9ab712ccf09670db5b1e00eb0b
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-04-30 20:39:53 +0200

    first idea splashscreen
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

---
+++ frontend/pages/SplashPage/Splash.styles.js
@@ -0,0 +1,26 @@
+
+import { StyleSheet } from "react-native";
+import { COLORS, FONT, SIZES } from "../../constants/theme";
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.background,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    video: {
+        width: '100%',
+        height: '70%',
+    },
+    text: {
+        color: COLORS.textWhite,
+        fontFamily: FONT.bold,
+        fontSize: 50,
+        textAlign: 'center'
+    },
+});
+
+export default styles;
