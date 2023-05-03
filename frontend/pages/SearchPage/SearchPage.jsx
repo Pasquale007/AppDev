bdc809bceb52c9d0d33a35fba1521da7483197e8
Pascal Thurow <73184231+Pasquale007@users.noreply.github.com>
2023-05-03 23:10:00 +0200

    Feature/16 99 push notification android (#32)
    * first idea push notification
    
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
    
    * Added some Comments to App.jsx Notifications
    
    * add dependencies
    
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
    
    * add icon for push notification and change name
    
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
    
    * Changed UUID to ExponentPushToken
    
    * reomve boiler code
    
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
    Co-authored-by: Orchi1904 <alexej1@web.de>

--- frontend/pages/SearchPage/SearchPage.jsx
+++ frontend/pages/SearchPage/SearchPage.jsx
@@ -13,6 +13,7 @@ import { useNavigation } from '@react-navigation/native';
 import flightData from '../../data/flightData.json';
 import ToastContainer from '../../components/ToastContainer/ToastContainer';
 import Toast from 'react-native-toast-message';
+import { sendPushNotification } from '../../pushNotification/pushNotification';
 
 export default function SearchPage() {
     const origins = flightData.map(dataset => dataset.origin);
@@ -81,7 +82,8 @@ export default function SearchPage() {
                 </ImageBackground>
                 <Button
                     text={"Suche"}
-                    onClick={() => {
+                    onClick={async () => {
+                        //await sendPushNotification('ExponentPushToken[Ef5M2qFl2bYdnWJG_LfS9m]');
                         if (!startAirport) {
                             Toast.show({
                                 type: "error",
@@ -115,8 +117,8 @@ export default function SearchPage() {
                                 'ignoredDestinations': '',
                                 'outFromDate': dateSpan?.from?.toISOString().split('T')[0],
                                 'outToDate': dateSpan?.until?.toISOString().split('T')[0],
-                                'lengthMin': duration?.start || undefined,
-                                'lengthMax': duration?.end || undefined
+                                'lengthMin': !flexible ? duration?.start : undefined,
+                                'lengthMax': !flexible ? duration?.end : undefined
                             }
                         });
                     }}
--- frontend/setup.js
+++ frontend/setup.js
@@ -1,5 +1,1 @@
-//Needed so tests can use crypto and wont crash
-import { Crypto } from "@peculiar/webcrypto"
 
-//eslint-disable-next-line no-undef
-global.crypto = new Crypto()
