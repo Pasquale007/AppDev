9e278d7926d1730417aa04be2c241db1943a6d00
Marc <marc.ruddigkeit@gmx.net>
2023-05-12 11:44:17 +0200

    Update Loading.jsx

--- frontend/pages/LoadingPage/Loading.jsx
+++ frontend/pages/LoadingPage/Loading.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, View } from 'react-native';
 import styles from './Splash.styles';
 import Giffile from '../../assets/loading-phrases/LoadingGif.gif';
 import { Text } from 'react-native';
+const LoadingPhrases1= useState(false);
 
 export default function LoadingScreen({ LoadingPhrases }) {
-
+  LoadingPhrases1=LoadingPhrases;
   useEffect(() => {
     setTimeout(() =>  4000);
   }, []);
@@ -17,7 +18,7 @@ export default function LoadingScreen({ LoadingPhrases }) {
         style={styles.video}
         resizeMode="cover"
       />
-      if (LoadingPhrases==true) {
+      if (LoadingPhrases1==true) {
         
       }else{
       <Text style={styles.text}>Flexi Flight</Text>
