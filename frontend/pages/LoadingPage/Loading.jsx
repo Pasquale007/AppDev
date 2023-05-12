1e6d1f8ce0e975bfe0fcf73755e0195306e53b36
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-05-12 11:58:49 +0200

    implement logic
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/pages/LoadingPage/Loading.jsx
+++ frontend/pages/LoadingPage/Loading.jsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Image, View } from 'react-native';
 import styles from './Splash.styles';
 import Giffile from '../../assets/loading-phrases/LoadingGif.gif';
 import { Text } from 'react-native';
 import LoadingPhrasesFile from '../../assets/loading-phrases/loadingPhrases';
-const LoadingPhrases1= useState(false);
 
-export default function LoadingScreen({ LoadingPhrases }) {
-  LoadingPhrases1=LoadingPhrases;
-  useEffect(() => {
-    setTimeout(() =>  4000);
-  }, []);
+export default function LoadingScreen({ loadingPhrases }) {
 
   return (
     <View style={styles.container}>
@@ -19,9 +14,9 @@ export default function LoadingScreen({ LoadingPhrases }) {
         style={styles.video}
         resizeMode="cover"
       />
-      {LoadingPhrases1
-      ? <Text>{LoadingPhrasesFile[Math.floor(Math.random() * LoadingPhrasesFile.length)].title}</Text>
-      : <Text style={styles.text}>Flexi Flight</Text>}
+      {loadingPhrases
+        ? <Text>{LoadingPhrasesFile[Math.floor(Math.random() * LoadingPhrasesFile.length)].title}</Text>
+        : <Text style={styles.text}>Flexi Flight</Text>}
     </View>
   );
 }
