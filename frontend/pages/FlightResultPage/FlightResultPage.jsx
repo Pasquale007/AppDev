d723c62fca2b3f3fa898b475ebff7f9dfe6f58df
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2023-05-31 22:59:54 +0200

    test
    Signed-off-by: Pasquale007 <73184231+Pasquale007@users.noreply.github.com>

--- frontend/pages/FlightResultPage/FlightResultPage.jsx
+++ frontend/pages/FlightResultPage/FlightResultPage.jsx
@@ -113,6 +113,6 @@ export default function FlightResultPage({ route }) {
                     <ToastContainer />
                 </ImageBackground>
             }
-        </SafeAreaView >
+        </SafeAreaView>
     );
 }
