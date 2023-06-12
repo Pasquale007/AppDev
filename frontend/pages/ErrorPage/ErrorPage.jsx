e2c4b585617a83b2dbf8ca0fa238067904f8834a
Marc <marc.ruddigkeit@gmx.net>
2023-06-12 08:55:46 +0200

    First try

--- frontend/pages/ErrorPage/ErrorPage.jsx
+++ frontend/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,22 @@
+import { View, Text, ImageBackground } from "react-native";
 import React from 'react';
 import styles from "./ErrorPage.style.js";
+import image from '../../assets/images/background.jpg';
 
 export function ErrorPage() {
 
 
     return (
-        <>
+        <View style={styles.container}>
+        <ImageBackground
+                    source={image}
+                    resizeMode="cover"
+                >
+        {/* <View style={styles.main}> */}
 
-        </>
+            <Text style={styles.errorText}>Kein Notification-Token erkannt</Text>
+            {/* </View> */}
+        </ImageBackground>
+        </View>
     )
 }
--- frontend/pages/ErrorPage/ErrorPage.style.js
+++ frontend/pages/ErrorPage/ErrorPage.style.js
@@ -1,8 +1,42 @@
 import { StyleSheet } from "react-native";
 import { COLORS, FONT, SIZES } from "../../constants/theme";
+import { Dimensions } from 'react-native'
 
 const styles = StyleSheet.create({
+   
+    flex: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: Dimensions.get('window').width,
+    },
+    main: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        margin: "5%",
+        marginTop: "30%",
+        color: COLORS.background,
+        height: Dimensions.get('window').height * 0.6,
 
+    },
+    container: {
+        backgroundColor: COLORS.background,
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    errorText: {
+        color: COLORS.textWhite,
+        fontFamily: FONT.regular,
+        fontSize: 20,
+        textAlign: 'center',
+        
+    },
 });
 
 export default styles;
