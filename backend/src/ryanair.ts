a74fdc40a153e7440a0ff7ea9f37d2007a9c43b4
Timm <63153223+timmsGithub@users.noreply.github.com>
2023-06-23 12:20:07 +0200

    Backend fix (#74)
    Co-authored-by: Timm <padd.hof@gmail.com>

--- backend/src/ryanair.ts
+++ backend/src/ryanair.ts
@@ -95,13 +95,13 @@ async function processDestination(origin: string, destination: string, outFromDa
 
     let outToDateLocal = new Date(outToDate) //WTF!?!?!?
     outToDateLocal = new Date(outToDateLocal.setHours(outToDateLocal.getHours() + 23)) //outToDate needs to be bigger than the inbound departure date which is only possible if it is 23:59 on the return day
-    outToDateLocal = new Date(outToDateLocal.sdetMinutes(outToDateLocal.getMinutes() + 59))
-
-    outFromDate = new Date(outFromDate.setHours(outFromDate.getHours() - timeShift)) // - timeShift so that i don't need to shift + every time we introduce a new var and compare it with the outFromDate/outToDate
+    outToDateLocal = new Date(outToDateLocal.setMinutes(outToDateLocal.getMinutes() + 59))
+    let outFromDateLocal = new Date(outFromDate)
+    outFromDateLocal = new Date(outFromDateLocal.setHours(outFromDateLocal.getHours() - timeShift)) // - timeShift so that i don't need to shift + every time we introduce a new var and compare it with the outFromDate/outToDate
     outToDateLocal = new Date(outToDateLocal.setHours(outToDateLocal.getHours() - timeShift))
 
     for (let i = 0; i < outbound.length; i++) {
-        if(outFromDate <= new Date(outbound[i].arrivalDate) && outToDateLocal >= new Date(outbound[i].arrivalDate) && outbound[i].unavailable == false && outbound[i].soldOut == false){
+        if(outFromDateLocal <= new Date(outbound[i].arrivalDate) && outToDateLocal >= new Date(outbound[i].arrivalDate) && outbound[i].unavailable == false && outbound[i].soldOut == false){
             //Flight available
             //Searching for back flight
             for (let j = i + lengthMin; j <= i + lengthMax ; j++) {
